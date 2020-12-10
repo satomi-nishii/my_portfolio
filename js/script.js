984d7d34172cb3e40961c826b9acfe534a6555ed
satomi-nishii <70808493+satomi-nishii@users.noreply.github.com>
2020-12-10 23:03:04 +0900

    軽微な修正

--- js/script.js
+++ js/script.js
@@ -38,22 +38,5 @@ $(function () {
         );
         return false;
     });
-
-    // formの入力確認
-    let $submit = $('#js-submit')
-    $('#js-form input, #js-form textarea').on('change', function() {
-        if (
-            $('#js-form input[type="text"]').val() !== "" &&
-            $('#js-form textarea').val() !== ""
-        ){
-            // 全て入力された時
-            $submit.prop('disabled', false)
-            $submit.addClass('-active')
-        } else {
-            // 全て入力されていない時
-            $submit.prop('disabled', true)
-            $submit.removeClass('-active')
-        }
-    })
     
 });
